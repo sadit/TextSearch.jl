b0d174ce151e885c0da3de74db2903ca58f5b901
Eric S. Tellez <donsadit@gmail.com>
2019-05-15 06:08:13 -0500

    adds quasi-adaptive entropy smoothing

--- src/entmodel.jl
+++ src/entmodel.jl
@@ -37,13 +37,17 @@ end
 function weighted_bow(model::EntModel, weighting::Type, data, modify_bow!::Function=identity)::Dict{Symbol, Float64}
     W = Dict{Symbol, Float64}()
     bag, maxfreq = compute_bow(model.config, data)
+    len = 0
+    for v in values(bag)
+        len += v
+    end
     bag = modify_bow!(bag)
     for (token, freq) in bag
         # get(model.vocab, token, UNKNOWN_TOKEN)
         # global_tokendata.freq == 0 && continue
         w = get(model.tokens, token, 0.0)
         if w > 0
-            W[token] = w
+            W[token] = w * freq / len
         end
         # ww = _weight(weighting, freq, maxfreq, model.n, global_tokendata.freq)
         # W[token] = w
