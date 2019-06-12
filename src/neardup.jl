f94bd73a43183b21fd138df96c9eadcd361e165d
Eric S. Tellez <donsadit@gmail.com>
2019-06-12 17:50:48 -0500

    fixes initial value for neardup

--- src/neardup.jl
+++ src/neardup.jl
@@ -4,7 +4,9 @@ function neardup(X::AbstractVector{T}, epsilon=0.1) where T
     invindex = InvIndex()
     res = KnnResult(1)
     L = zeros(Int, length(X))
+    D = zeros(Float64, length(X))
     L[1] = 1
+    D[1] = 0.0
     push!(invindex, 1, X[1])
     for i in 2:length(X)
         empty!(res)
@@ -13,10 +15,12 @@ function neardup(X::AbstractVector{T}, epsilon=0.1) where T
         if length(res) == 0 || first(res).dist > epsilon
             push!(invindex, i, x)
             L[i] = i
+            D[i] = 0.0
         else
             L[i] = first(res).objID
+            D[i] = first(res).dist
         end
     end
 
-    L
+    L, D
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -273,6 +273,6 @@ end
     model = fit(VectorModel, config, corpus)
     @show corpus[1:10]
     X = [vectorize(model, TfModel, x) |> normalize! for x in corpus]
-    XX = neardup(X, 0.2)
-    @test length(X) > length(unique(XX))
+    L, D  = neardup(X, 0.2)
+    @test length(X) > length(unique(L))
 end
