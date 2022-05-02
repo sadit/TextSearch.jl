938bf94df6f6bdab2f865580d16c5923d4dbaa98
Eric S. Tellez <donsadit@gmail.com>
2022-05-02 11:55:36 -0500

    adds tests for dvec

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,47 @@ const text2 = "a b c d e f g h i j k l m n o p q"
 const corpus = ["hello world :)", "@user;) excellent!!", "#jello world."]
 
 
+@testset "DVEC" begin
+    cmpex(u, v) = abs(u[1] - v[1]) < 1e-3 && u[2] == v[2]
+
+    aL = []
+    AL = []
+    for i in 1:10
+        A = rand(300)
+        B = rand(300)
+        a = SVEC(k => v for (k, v) in enumerate(A))
+        b = SVEC(k => v for (k, v) in enumerate(B))
+
+        @test abs(norm(A) - norm(a)) < 1e-3
+        @test abs(norm(B) - norm(b)) < 1e-3
+        normalize!(A); normalize!(a)
+        normalize!(B); normalize!(b)
+        @test abs(norm(a) - 1.0) < 1e-3
+        @test abs(norm(b) - 1.0) < 1e-3
+
+        @test abs(dot(a, b) - dot(A, B)) < 1e-3
+        @test abs(maximum(a) - maximum(A)) < 1e-3
+        @test abs(minimum(a) - minimum(A)) < 1e-3
+
+        
+        @test cmpex(findmax(a), findmax(A))
+        @test cmpex(findmin(a), findmin(A))
+
+        push!(aL, a)
+        push!(AL, A)
+    end
+
+    @test (norm(sum(AL)) - norm(sum(aL))) < 1e-3
+
+    adist = AngleDistance()
+    cdist = CosineDistance()
+
+    for i in 1:length(aL)-1
+        @test abs(evaluate(adist, aL[i], aL[i+1]) - evaluate(adist, AL[i], AL[i+1])) < 1e-3
+        @test abs(evaluate(cdist, aL[i], aL[i+1]) - evaluate(cdist, AL[i], AL[i+1])) < 1e-3
+    end
+end
+
 @testset "individual tokenizers" begin
     m = Tokenizer(TextConfig(nlist=[1]))
     @test tokenize(m, text0) == ["@user", ";)", "#jello", ".", "world"]
